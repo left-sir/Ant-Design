34a206994091c7eda4372ce15f296f6de89645d5
left-sir <2553143810@qq.com>
2019-08-05 11:26:10 +0800

    第二次

--- antdesign/src/components/HelloWorld.vue
+++ antdesign/src/components/HelloWorld.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style scoped>
+h1, h2 {
+  font-weight: normal;
 }
 ul {
   list-style-type: none;
