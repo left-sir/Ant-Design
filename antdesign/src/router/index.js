123fea2335ba0c463faabad3fb81fc2d2dcc4cd8
left-sir <2553143810@qq.com>
2019-08-06 18:08:37 +0800

    qqq

--- antdesign/src/router/index.js
+++ antdesign/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import First from '@/components/nav/First'
+import table from '@/components/Table/table'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'First',
+      component: First
+    }
+    ,
+    {
+      path: '/table',
+      name: 'table',
+      component: table
     }
   ]
 })
